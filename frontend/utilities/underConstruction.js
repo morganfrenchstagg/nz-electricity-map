1c382c5b0f9ceca8932e7bde4118bd8915e48915
Morgan <morgan.french-stagg@authsignal.com>
2026-02-16 19:41:41 +1300

    kowhai park online end june

--- frontend/utilities/underConstruction.js
+++ frontend/utilities/underConstruction.js
@@ -91,7 +91,7 @@ export const underConstruction = [
         "capacityMW": 150,
         "capacityMWp": 168,
         "yearlyGenerationGWh": 275,
-        "openBy": "2026-05-31",
+        "openBy": "2026-06-30",
         "location": {
             "lat": -43.49277380784132, //todo - refine this down further
             "long": 172.50953626586775
