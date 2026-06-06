d589e6835072cbb3a2441c8eb3f0b8deab02f375
Morgan French-Stagg <morgan@frenchsta.gg>
2026-06-06 20:32:12 +1200

    remove backend, now migrated to cloudflare workers

--- backend/services/SubstationDescriptions.py
+++
@@ -1,8 +0,0 @@
-import json
-
-fileName = 'data/substations.json'
-
-class SubstationDescriptions:
-    def __init__(self) -> None:
-        with open(fileName) as f:
-            self.descriptions = json.load(f)
