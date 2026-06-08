83f0b98218ce492f41df066774495fae62fdbfb6
Morgan French-Stagg <morgan@frenchsta.gg>
2026-06-08 17:23:41 +1200

    implement caching of price data

--- frontend/utilities/api.js
+++ frontend/utilities/api.js
@@ -8,8 +8,9 @@ let prodUrl = (path) => `https://api.frenchsta.gg/v1/${path}`;
 let isProd = (new URLSearchParams(window.location.search)).get('local') !== 'true';
 
 let timeseriesGenerationDataCache = {};
+let timeseriesPriceDataCache = {};
 
-export async function fetchJson(path){
+export async function fetchJson(path) {
     let data = {};
 
     if (isProd) {
@@ -18,24 +19,24 @@ export async function fetchJson(path){
         data = await fetch(localUrl(path));
     }
 
-    if(data.status === 200){
+    if (data.status === 200) {
         return data.json();
     }
 
     return {};
 }
 
-function formatDate(date){
+function formatDate(date) {
     return `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`;
 }
 
 const spinnerHtml = '<div class="spinner-border spinner-border-sm text-primary" role="status"><span class="visually-hidden">Loading...</span></div>';
 
-export async function getTimeseriesGenerationData(date){
+export async function getTimeseriesGenerationData(date) {
     var currentTimeFormatted = formatDate(getCurrentTimeInNZ());
     var dateStr = formatDate(date);
 
-    if(timeseriesGenerationDataCache[dateStr] && (dateStr != currentTimeFormatted)){
+    if (timeseriesGenerationDataCache[dateStr] && (dateStr != currentTimeFormatted)) {
         return timeseriesGenerationDataCache[dateStr];
     }
 
@@ -46,24 +47,33 @@ export async function getTimeseriesGenerationData(date){
 
     timeseriesGenerationDataCache[dateStr] = json;
 
-    return json ;
+    return json;
 }
 
-export async function getTimeseriesPriceData(date){
+export async function getTimeseriesPriceData(date) {
+    var currentTimeFormatted = formatDate(getCurrentTimeInNZ());
+    var dateStr = formatDate(date);
+
+    if (timeseriesPriceDataCache[dateStr] && (dateStr != currentTimeFormatted)) {
+        return timeseriesPriceDataCache[dateStr];
+    }
+
     var dateStr = formatDate(date);
     const response = await fetch(`https://api.electricitymap.frenchsta.gg/v1/dispatch/legacy/history/price/${dateStr}`);
     const json = response.json();
 
+    timeseriesPriceDataCache[dateStr] = json;
+
     return json;
 }
 
-export async function getLiveGenerationData(){
+export async function getLiveGenerationData() {
     const response = await fetch('https://api.electricitymap.frenchsta.gg/v1/dispatch/legacy/generators');
 
     return response.json();
 }
 
-export async function getTimeseriesOfferData(date){
+export async function getTimeseriesOfferData(date) {
     console.log(date);
     var dateStr = formatDate(date);
     const result = fetch(`https://api.electricitymap.frenchsta.gg/v1/offers/${dateStr}`);
