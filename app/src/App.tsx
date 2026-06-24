35e3485b4533c6cce98caa06f6de31c7da554522
Morgan <morgan.french-stagg@authsignal.com>
2026-06-25 11:42:53 +1200

    don't force the grid panel open

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -13,7 +13,6 @@ export default function App() {
 
   const closeNode = useCallback(() => {
     setSelectedNode(null)
-    setGridPanelVisible(true)
   }, [])
 
   const [dateMode, setDateMode] = useState<DateMode>(() => {
